17a227baa2f745563cc0397f94bfb1e9c04b334c
and <Andrey Kochanov>
2023-06-06 11:44:57 +0300

    Сделана вёрстка страниц:  Авторизация: "/"  Регистрация: "/create"  Главная с чатом: "/chat"  Профиль: "/profile"
    Настроил vitejs и шаблонизатор handlebars

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("./dist"));
+
+app.get("*", (req, res) => {
+  res.sendFile("./dist/index.html", {
+    root: dirname(fileURLToPath(import.meta.url)),
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Super on ${PORT}`);
+});
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as loginPage } from "./loginPage/index.tmpl";
+export { default as registRationPage } from "./registrationPage/index.tmpl";
+export { default as profilePage } from "./profilePage/index.tmpl";
+export { default as mainPage } from "./mainPage/index.tmpl";
+export { default as notFound } from "./notFoundPage/index.tmpl";
