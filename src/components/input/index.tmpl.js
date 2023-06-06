06b3d18d8be7446d433cd6b8776a8de3a1bd0bd1
and <Andrey Kochanov>
2023-06-06 11:44:57 +0300

    Сделана вёрстка страниц:  Авторизация: "/"  Регистрация: "/create"  Главная с чатом: "/chat"  Профиль: "/profile"
    Настроил vitejs и шаблонизатор handlebars

---
+++ src/components/input/index.tmpl.js
@@ -0,0 +1,19 @@
+import Handlebars from "handlebars";
+import getProps from "../../utils/getProps";
+
+export default Handlebars.registerPartial("input", (p, o) => {
+  const props = getProps(p);
+  let inputEl = `<input  class="input ${p.class}" ${props}/>`;
+  if (p?.title) {
+    inputEl = `
+        <div  class="inputWrapper">
+            <div class="text">
+                ${p.title}
+            </div>
+            <input  class="input ${p.class}" ${props}/>
+        </div>
+        `;
+  }
+
+  return inputEl;
+});
